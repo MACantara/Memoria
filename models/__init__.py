d6dafdd9e6945283b2e0941383d2e2a403268c85
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-03-07 00:54:55 +0800

    Add FlashcardSet and FlashcardGenerator classes for unique flashcard management and generation

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 from .flashcard_deck import FlashcardDecks
-from .flashcard import Flashcards
+from .flashcard import Flashcards, FlashcardSet, FlashcardGenerator
 
 __all__ = ['db', 'FlashcardDecks', 'Flashcards']
